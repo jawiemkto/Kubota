f6c7cd39ff3a54cd5635ea33fc4377b137187860
zbooyx <pawel.zboinski@neoteric.eu>
2019-01-25 16:32:19 +0100

    added  addYourCudtomSVG div

--- src/app/logos/logos.component.ts
+++ src/app/logos/logos.component.ts
@@ -11,6 +11,9 @@ export class LogosComponent implements OnInit {
     {"title": "bla", "logoName": "iconfinder_deer_3406417.svg"},
     {"title": "blagla", "logoName": "iconfinder_sloth_3406421.svg"},
     {"title": "blagladla", "logoName": "iconfinder_squirrel_3406420.svg"},
+    {"title": "blagladlaff", "logoName": "iconfinder_crocodile_3406445.svg"},
+    {"title": "blagladlaff", "logoName": "iconfinder_fox_3406441.svg"},
+    {"title": "blagladlaff", "logoName": "iconfinder_meerkat_3406432.svg"},
   ];
 
   constructor() { }
