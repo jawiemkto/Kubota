56ae7b4d11489cc1a7997d0e2d2b7b716551e8bc
Kamil Narkiewicz <kamil.narkiewicz@neoteric.eu>
2019-01-25 17:36:25 +0100

    feat(): add binding to svg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CanvasComponent } from './canvas/canvas.component';
 import { LogosComponent } from './logos/logos.component';
-import { CustomizationComponent } from './customization/customization.component';
 import { LogoComponent } from './logos/logo/logo.component';
 import { MaterialModule } from './material/material.module';
 import { ColorPickerModule } from 'ngx-color-picker';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -17,14 +17,14 @@ import { ColorPickerModule } from 'ngx-color-picker';
     HeaderComponent,
     CanvasComponent,
     LogosComponent,
-    CustomizationComponent,
     LogoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialModule,
-    ColorPickerModule
+    ColorPickerModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
