cf1f42ab1dd7992ada061299565f5287c3cdd72f
Kamil Narkiewicz <kamil.narkiewicz@neoteric.eu>
2019-01-25 13:26:10 +0100

    flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CanvasComponent } from './canvas/canvas.component';
+import { LogosComponent } from './logos/logos.component';
+import { CustomizationComponent } from './customization/customization.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CanvasComponent,
+    LogosComponent,
+    CustomizationComponent,
+    FlexLayoutModule
   ],
   imports: [
     BrowserModule,
